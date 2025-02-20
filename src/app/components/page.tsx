0498da177fa6a941586032ec2c5dc6fbbe890daf
Wahyu Kurniawan <ayungavis@gmail.com>
2025-02-21 04:22:02 +0700

    ⚡️ feat: add aspect ratio component

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,4 +1,5 @@
 import { Terminal } from 'lucide-react';
+import Image from 'next/image';
 
 import { ModeToggle } from '@/components/toggle-mode';
 import {
@@ -19,6 +20,7 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
 import { Button } from '@/components/ui/button';
 
 const ComponentsPage = () => {
@@ -95,6 +97,19 @@ const ComponentsPage = () => {
               </AlertDialogFooter>
             </AlertDialogContent>
           </AlertDialog>
+        </div>
+
+        {/* Aspect Ratio */}
+        <div className="flex w-full max-w-4xl flex-col space-y-4">
+          <h4 className="text-lg font-medium">Aspect Ratio</h4>
+          <AspectRatio className="bg-muted" ratio={16 / 9}>
+            <Image
+              fill
+              alt="Photo by Drew Beamer"
+              className="h-full w-full rounded-md object-cover"
+              src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+            />
+          </AspectRatio>
         </div>
 
         {/* Button */}
