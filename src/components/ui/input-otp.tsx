e768e66e66c833ceb3474abd2998879c8834db4c
Wahyu Kurniawan <ayungavis@gmail.com>
2025-02-21 11:19:40 +0700

    🐛 fix: linter

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import * as React from 'react';
 import { OTPInput, OTPInputContext } from 'input-otp';
 import { MinusIcon } from 'lucide-react';
+import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
@@ -15,12 +15,12 @@ function InputOTP({
 }) {
   return (
     <OTPInput
-      data-slot="input-otp"
+      className={cn('disabled:cursor-not-allowed', className)}
       containerClassName={cn(
         'flex items-center gap-2 has-disabled:opacity-50',
         containerClassName,
       )}
-      className={cn('disabled:cursor-not-allowed', className)}
+      data-slot="input-otp"
       {...props}
     />
   );
@@ -29,8 +29,8 @@ function InputOTP({
 function InputOTPGroup({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
-      data-slot="input-otp-group"
       className={cn('flex items-center', className)}
+      data-slot="input-otp-group"
       {...props}
     />
   );
@@ -48,12 +48,12 @@ function InputOTPSlot({
 
   return (
     <div
-      data-slot="input-otp-slot"
-      data-active={isActive}
       className={cn(
         'border-input ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 relative flex h-9 w-9 items-center justify-center border-y border-r text-sm shadow-xs transition-all first:rounded-l-md first:border-l last:rounded-r-md data-[active=true]:z-10 data-[active=true]:ring-4 data-[active=true]:outline-1',
         className,
       )}
+      data-active={isActive}
+      data-slot="input-otp-slot"
       {...props}
     >
       {char}
@@ -74,4 +74,4 @@ function InputOTPSeparator({ ...props }: React.ComponentProps<'div'>) {
   );
 }
 
-export { InputOTP, InputOTPGroup, InputOTPSlot, InputOTPSeparator };
+export { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot };
