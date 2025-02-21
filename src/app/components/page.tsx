76f8bf6802a6d36df144fa84dbe4888215613f23
Wahyu Kurniawan <ayungavis@gmail.com>
2025-02-21 10:34:24 +0700

    ⚡️ feat: add popover component

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -83,6 +83,11 @@ import {
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import {
   Select,
   SelectContent,
   SelectGroup,
@@ -522,6 +527,60 @@ const ComponentsPage = () => {
               type="input-with-label"
             />
           </div>
+        </div>
+
+        {/* Popover */}
+        <div className="flex flex-col space-y-4">
+          <h4 className="text-lg font-medium">Popover</h4>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="outline">Open popover</Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+              <div className="grid gap-4">
+                <div className="space-y-2">
+                  <h4 className="leading-none font-medium">Dimensions</h4>
+                  <p className="text-muted-foreground text-sm">
+                    Set the dimensions for the layer.
+                  </p>
+                </div>
+                <div className="grid gap-2">
+                  <div className="grid grid-cols-3 items-center gap-4">
+                    <Label htmlFor="width">Width</Label>
+                    <Input
+                      className="col-span-2 h-8"
+                      defaultValue="100%"
+                      id="width"
+                    />
+                  </div>
+                  <div className="grid grid-cols-3 items-center gap-4">
+                    <Label htmlFor="maxWidth">Max. width</Label>
+                    <Input
+                      className="col-span-2 h-8"
+                      defaultValue="300px"
+                      id="maxWidth"
+                    />
+                  </div>
+                  <div className="grid grid-cols-3 items-center gap-4">
+                    <Label htmlFor="height">Height</Label>
+                    <Input
+                      className="col-span-2 h-8"
+                      defaultValue="25px"
+                      id="height"
+                    />
+                  </div>
+                  <div className="grid grid-cols-3 items-center gap-4">
+                    <Label htmlFor="maxHeight">Max. height</Label>
+                    <Input
+                      className="col-span-2 h-8"
+                      defaultValue="none"
+                      id="maxHeight"
+                    />
+                  </div>
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
         </div>
 
         {/* Select */}
